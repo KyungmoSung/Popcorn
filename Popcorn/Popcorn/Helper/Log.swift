e68ec1e7cac4bbdc97a54883886998f11b652809
km.sung <km.sung@greencar.co.kr>
2020-10-29 17:03:45 +0900

    Log 출력시 현재시간 표기

--- Popcorn/Popcorn/Helper/Log.swift
+++ Popcorn/Popcorn/Helper/Log.swift
@@ -15,37 +15,50 @@ class Log {
         case e = "[‼️]" // error
     }
     
+    private static var dateFormat = "yyyy-MM-dd hh:mm:ss"
+    private static var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = dateFormat
+        formatter.locale = Locale.current
+        formatter.timeZone = TimeZone.current
+        return formatter
+    }
+    
+    private static var logDate: String {
+        return dateFormatter.string(from: Date())
+    }
+    
     private class func logInfo(file: String, function: String, line: Int) -> String {
         let fileName = file.split(separator: "/").last ?? ""
         let funcName = function.split(separator: "(").first ?? ""
-        return "[\(fileName)] \(funcName)(\(line)): "
+        return "[\(fileName)] \(funcName)(\(line)):"
     }
     
     class func i(_ msg: Any, file: String = #file, function: String = #function, line: Int = #line) {
         #if DEBUG
         let info = logInfo(file: file, function: function, line: line)
-        print(LogEvent.i.rawValue + info + "\(msg)")
+        print("\(logDate) \(LogEvent.i.rawValue) \(info) \(msg)")
         #endif
     }
     
     class func d(_ msg: Any, file: String = #file, function: String = #function, line: Int = #line) {
         #if DEBUG
         let info = logInfo(file: file, function: function, line: line)
-        print(LogEvent.d.rawValue + info + "\(msg)")
+        print("\(logDate) \(LogEvent.d.rawValue) \(info) \(msg)")
         #endif
     }
     
     class func w(_ msg: Any, file: String = #file, function: String = #function, line: Int = #line) {
         #if DEBUG
         let info = logInfo(file: file, function: function, line: line)
-        print(LogEvent.w.rawValue + info + "\(msg)")
+        print("\(logDate) \(LogEvent.w.rawValue) \(info) \(msg)")
         #endif
     }
     
     class func e(_ msg: Any, file: String = #file, function: String = #function, line: Int = #line) {
         #if DEBUG
         let info = logInfo(file: file, function: function, line: line)
-        print(LogEvent.e.rawValue + info + "\(msg)")
+        print("\(logDate) \(LogEvent.e.rawValue) \(info) \(msg)")
         #endif
     }
 }
