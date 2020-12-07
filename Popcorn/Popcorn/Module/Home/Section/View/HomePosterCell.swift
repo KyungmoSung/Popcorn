37ca6d7252b89461c8fbb223042a7a5aaf3a791c
Sung Kyungmo <skm280@naver.com>
2020-12-07 20:26:34 +0900

    포스터셀 평점 추가, Blur효과 적용

--- Popcorn/Popcorn/Module/Home/Section/View/HomePosterCell.swift
+++ Popcorn/Popcorn/Module/Home/Section/View/HomePosterCell.swift
@@ -10,7 +10,18 @@ import UIKit
 class HomePosterCell: UICollectionViewCell {
     @IBOutlet private weak var titleLb: UILabel!
     @IBOutlet private weak var posterIv: UIImageView!
+    @IBOutlet weak var voteView: CustomView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let blurEffect = UIBlurEffect(style: .regular)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = voteView.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        voteView.addSubview(blurEffectView)
+    }
+
     var title: String? {
         didSet {
             titleLb.text = title
