fa10562a152b312be57f53159e536061d1f8341a
Sung Kyungmo <skm280@naver.com>
2021-01-15 18:22:16 +0900

    네비게이션 바 Back, Search 커스텀 버튼 추가

--- Popcorn/Popcorn/Module/Common/BaseViewController.swift
+++ Popcorn/Popcorn/Module/Common/BaseViewController.swift
@@ -29,5 +29,41 @@ class BaseViewController: UIViewController {
         self.title = title
         navigationBarIsHidden = !navigationBar
         tabBarIsHidden = !tabBar
+        
+        // 기본 뒤로가기 버튼 제거
+        navigationItem.setHidesBackButton(true, animated: false);
+        
+        // 좌측 버튼 아이템 추가
+        let leftBarBtnItemView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        
+        let leftBtn = UIButton(type: .custom)
+        leftBarBtnItemView.addSubview(leftBtn)
+        leftBtn.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        leftBtn.setImage(UIImage(named: "icBack"), for: .normal)
+        leftBtn.addTarget(self, action: #selector(didTapNavigationBackBtn), for: .touchUpInside)
+        leftBtn.tintColor = .label
+        
+        let leftBarButtonItem = UIBarButtonItem(customView: leftBarBtnItemView)
+        navigationItem.leftBarButtonItem = leftBarButtonItem
+        
+        let rightBarBtnItemView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 30))
+        
+        let rightBtn = UIButton(type: .custom)
+        rightBarBtnItemView.addSubview(rightBtn)
+        rightBtn.frame = CGRect(x: 0, y: 0, width: 30, height: 30) // 간격 20으로 맞추기 위해 x축으로 10 이동
+        rightBtn.setImage(UIImage(named: "icSearch"), for: .normal)
+        rightBtn.addTarget(self, action: #selector(didTapNavigationSearchBtn), for: .touchUpInside)
+        rightBtn.tintColor = .label
+        
+        let rightBarButtonItem = UIBarButtonItem(customView: rightBarBtnItemView)
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+
+    @objc func didTapNavigationBackBtn() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func didTapNavigationSearchBtn() {
+        
     }
 }
