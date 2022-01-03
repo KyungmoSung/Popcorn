b4742ed9e1f823234477ba2c9e10e10dea6ec525
Sung Kyungmo <skm280@naver.com>
2022-01-03 14:30:32 +0900

    feat: gitignore 적용

--- Popcorn/Pods/Target Support Files/Pods-Popcorn-PopcornUITests/Pods-Popcorn-PopcornUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Popcorn_PopcornUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Popcorn_PopcornUITests
-@end
--- Popcorn/Pods/Target Support Files/Pods-Popcorn/Pods-Popcorn-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Popcorn : NSObject
-@end
-@implementation PodsDummy_Pods_Popcorn
-@end
